c4dce4729f5dddb084c69c72f0ad7da6f05ada20
Jiří Strnadel <Juzba88@gmail.com>
2025-02-14 23:47:02 +0100

    done?

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,37 +1,52 @@
-import './NavMenu.scss';
-import logo from '../images/netflix.png';
-import { FaAlignJustify } from 'react-icons/fa6';
-import { useEffect, useState } from 'react';
+import "./NavMenu.scss";
+import logo from "../images/netflix.png";
+import { FaAlignJustify } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 const NavMenu = () => {
-    const [size, setSize] = useState(0)
+	const [size, setSize] = useState(window.innerWidth);
+	const [openMenu, setOpenMenu] = useState(false);
+    
 
-useEffect(()=>{
-    setSize(window.innerWidth)
-    console.log("useeffect")
-})
+	useEffect((timer) => {
+		window.addEventListener("resize", () => {
+            if(!timer){
+               timer = setTimeout(() => {
+                    console.log("sdffsd");
+                    setSize(window.innerWidth);
+                }, 250);
+            }
+		});
+	
+	}, [size]);
 
-    return (
-        <div>
-            <div className='navmenu'>
-                <img src={logo} alt='' />
-                <div className='menu'>
-                    <p>{size}</p>
-                    <a href='sf'>Domů</a>
-                    <a href='sf'>Pořady</a>
-                    <a href='sf'>Filmy</a>
-                    <a href='sf'>Můj seznam</a>
-                    <button>{FaAlignJustify()}</button>
-                </div>
-            </div>
-            <div className='smallmenu'>
-            <a href='sf'>Domů</a>
-                <a href='sf'>Pořady</a>
-                <a href='sf'>Filmy</a>
-                <a href='sf'>Můj seznam</a>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div className='navmenu'>
+				<img src={logo} alt='' />
+				{size > 500 ? (
+					<div className='menu'>
+						<a href='sf'>Domů</a>
+						<a href='sf'>Pořady</a>
+						<a href='sf'>Filmy</a>
+						<a href='sf'>Můj seznam</a>
+					</div>
+				) : (
+					<button onClick={() => setOpenMenu(!openMenu)} className='btn-menu'>
+						{FaAlignJustify()}
+					</button>
+				)}
+			</div>
+			{openMenu && size < 500 && (
+				<div className='smallmenu'>
+					<a href='sf'>Domů</a>
+					<a href='sf'>Pořady</a>
+					<a href='sf'>Filmy</a>
+					<a href='sf'>Můj seznam</a>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default NavMenu;
