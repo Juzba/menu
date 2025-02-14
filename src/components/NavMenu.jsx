8ff97751ea433c68e5ae4346848ad2a76e8038a1
Jiří Strnadel <Juzba88@gmail.com>
2025-02-14 21:48:34 +0100

    update

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,17 +1,34 @@
 import './NavMenu.scss';
 import logo from '../images/netflix.png';
-import { FaAlignJustify } from "react-icons/fa6";
+import { FaAlignJustify } from 'react-icons/fa6';
+import { useEffect, useState } from 'react';
 
 const NavMenu = () => {
+    const [size, setSize] = useState(0)
+
+useEffect(()=>{
+    setSize(window.innerWidth)
+    console.log("useeffect")
+})
+
     return (
-        <div className='navmenu'>
-            <img src={logo} alt='' />
-            <div className='menu'>
-                <a href='sf'>Domů</a>
+        <div>
+            <div className='navmenu'>
+                <img src={logo} alt='' />
+                <div className='menu'>
+                    <p>{size}</p>
+                    <a href='sf'>Domů</a>
+                    <a href='sf'>Pořady</a>
+                    <a href='sf'>Filmy</a>
+                    <a href='sf'>Můj seznam</a>
+                    <button>{FaAlignJustify()}</button>
+                </div>
+            </div>
+            <div className='smallmenu'>
+            <a href='sf'>Domů</a>
                 <a href='sf'>Pořady</a>
                 <a href='sf'>Filmy</a>
                 <a href='sf'>Můj seznam</a>
-                <button>{FaAlignJustify()}</button>
             </div>
         </div>
     );
