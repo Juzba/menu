b66e6dc2f31fb8840c787b4253c9da5aef94ca8a
Jiří Strnadel <Juzba88@gmail.com>
2025-02-15 22:30:26 +0100

    hotovo

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,42 +1,23 @@
-/** @format */
-
 import "./NavMenu.scss";
 import logo from "../images/netflix.png";
 import { FaAlignJustify } from "react-icons/fa6";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import data from "../data";
 
 const NavMenu = () => {
-	const [size, setSize] = useState(window.innerWidth);
-	const [openMenu, setOpenMenu] = useState(false);
-
-	useEffect(
-		(timer) => {
-			window.addEventListener("resize", () => {
-				if (!timer) {
-					timer = setTimeout(() => {
-						console.log("sdffsd");
-						setSize(window.innerWidth);
-						if (openMenu && size > 550) setOpenMenu(false);
-					}, 100);
-				}
-			});
-		},
-		[size, openMenu]
-	);
+	const [showMenu, setOpenMenu] = useState(false);
 
 	return (
 		<div className="navmenu">
 			<div className="container">
 				<img src={logo} alt="" />
-				<button className="btn-menu"> {FaAlignJustify()}</button>
+				<button onClick={() => setOpenMenu(!showMenu)}> {FaAlignJustify()}</button>
 			</div>
-			<ul className="menu">
+
+			<ul className={`${showMenu ? "nav-list show" : "nav-list hide"}`}>
 				{data.map(({ id, url, text }) => (
-					<li>
-						<a key={id} href={url}>
-							{text}
-						</a>
+					<li key={id}>
+						<a href={url}>{text}</a>
 					</li>
 				))}
 			</ul>
