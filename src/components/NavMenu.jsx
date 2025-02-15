76c36358a5744c83ae62779da4e4be2a6764dd6e
Jiří Strnadel <Juzba88@gmail.com>
2025-02-15 14:26:05 +0100

    pred zmenou

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,49 +1,62 @@
+/** @format */
+
 import "./NavMenu.scss";
 import logo from "../images/netflix.png";
 import { FaAlignJustify } from "react-icons/fa6";
 import { useEffect, useState } from "react";
+import data from "../data";
 
 const NavMenu = () => {
 	const [size, setSize] = useState(window.innerWidth);
 	const [openMenu, setOpenMenu] = useState(false);
-    
 
-	useEffect((timer) => {
-		window.addEventListener("resize", () => {
-            if(!timer){
-               timer = setTimeout(() => {
-                    console.log("sdffsd");
-                    setSize(window.innerWidth);
-                }, 250);
-            }
-		});
-	
-	}, [size]);
+	useEffect(
+		(timer) => {
+			window.addEventListener("resize", () => {
+				if (!timer) {
+					timer = setTimeout(() => {
+						console.log("sdffsd");
+						setSize(window.innerWidth);
+						if (openMenu && size > 550) setOpenMenu(false);
+					}, 100);
+				}
+			});
+		},
+		[size, openMenu]
+	);
 
 	return (
 		<div>
-			<div className='navmenu'>
-				<img src={logo} alt='' />
-				{size > 500 ? (
-					<div className='menu'>
-						<a href='sf'>Domů</a>
-						<a href='sf'>Pořady</a>
-						<a href='sf'>Filmy</a>
-						<a href='sf'>Můj seznam</a>
-					</div>
+			<div className="navmenu">
+				<img src={logo} alt="" />
+
+				{size > 550 ? (
+					<ul className="menu">
+						{data.map(({ id, url, text }) => {
+							return (
+								<a key={id} href={url}>
+									{text}
+								</a>
+							);
+						})}
+					</ul>
 				) : (
-					<button onClick={() => setOpenMenu(!openMenu)} className='btn-menu'>
+					<button onClick={() => setOpenMenu(!openMenu)} className="btn-menu">
 						{FaAlignJustify()}
 					</button>
 				)}
 			</div>
-			{openMenu && size < 500 && (
-				<div className='smallmenu'>
-					<a href='sf'>Domů</a>
-					<a href='sf'>Pořady</a>
-					<a href='sf'>Filmy</a>
-					<a href='sf'>Můj seznam</a>
-				</div>
+
+			{openMenu && size < 550 && (
+				<ul className="smallmenu">
+					{data.map(({ id, url, text }) => {
+						return (
+							<a key={id} href={url}>
+								{text}
+							</a>
+						);
+					})}
+				</ul>
 			)}
 		</div>
 	);
